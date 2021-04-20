d7a70d5d2de890cdf465e582065c3b700b513e24
JohnRain55 <77028227+JohnRain55@users.noreply.github.com>
2021-04-21 01:04:12 +0300

    radio_buttons_page.py created, should_be_elemnts 2/2

--- pages/input_forms/radio_buttons_page.py
+++ pages/input_forms/radio_buttons_page.py
@@ -11,6 +11,15 @@ class RadioButtonsPage(BasePage):
         self.should_be_button_check_male_or_female()
         self.should_be_emerging_text()
         self.should_text_be_undisplayed()
+        self.should_be_radio_sex_male()
+        self.should_be_radio_sex_female()
+        self.should_be_radio_age_0_5()
+        self.should_be_radio_age_5_15()
+        self.should_be_radio_age_15_50()
+        self.should_be_button_get_values()
+        self.should_be_sex_values()
+        self.should_not_be_age_value()
+        self.should_be_undisplayed_sex()
 
     def should_be_radio_button_male(self):
         assert self.is_element_present(*RadioButtonPageLocators.RADIO_BUTTON_MALE), \
@@ -31,6 +40,42 @@ class RadioButtonsPage(BasePage):
     def should_text_be_undisplayed(self):
         assert not self.is_element_displayed(*RadioButtonPageLocators.CHECKING_TEXT), \
             "Checking text is displayed, but should not"
+
+    def should_be_radio_sex_male(self):
+        assert self.is_element_present(*RadioButtonPageLocators.RADIO_SEX_MALE), \
+            'Radio sex male is not present, but should'
+
+    def should_be_radio_sex_female(self):
+        assert self.is_element_present(*RadioButtonPageLocators.RADIO_SEX_FEMALE), \
+            'Radio sex female is not present, but should'
+
+    def should_be_radio_age_0_5(self):
+        assert self.is_element_present(*RadioButtonPageLocators.RADIO_AGE_GROUP_0_5), \
+            'Radio age group 0 - 5  is not present, but should'
+
+    def should_be_radio_age_5_15(self):
+        assert self.is_element_present(*RadioButtonPageLocators.RADIO_AGE_GROUP_5_15), \
+            'Radio age group 5 - 15  is not present, but should'
+
+    def should_be_radio_age_15_50(self):
+        assert self.is_element_present(*RadioButtonPageLocators.RADIO_AGE_GROUP_15_50), \
+            'Radio age group 15 - 50  is not present, but should'
+
+    def should_be_button_get_values(self):
+        assert self.is_element_present(*RadioButtonPageLocators.BUTTON_GET_VALUES), \
+            "Button get values is not present, but should"
+
+    def should_be_sex_values(self):
+        assert self.is_element_present(*RadioButtonPageLocators.SEX_VALUE), \
+            "Sex values is not present, but should"
+
+    def should_not_be_age_value(self):
+        assert not self.is_element_present(*RadioButtonPageLocators.AGE_VALUE), \
+            "Age values is present, but should not"
+
+    def should_be_undisplayed_sex(self):
+        assert not self.is_element_displayed(*RadioButtonPageLocators.SEX_VALUE), \
+            "Sex values  displayed, but should not"
 
     def quest_one(self):
         button_check = self.browser.find_element(*RadioButtonPageLocators.BUTTON_CHECK_MALE_OR_FEMALE)
