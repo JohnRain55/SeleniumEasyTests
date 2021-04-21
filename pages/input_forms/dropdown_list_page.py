85aec6dd170d6bcf51c9ec514d663bda19a380e5
JohnRain55 <77028227+JohnRain55@users.noreply.github.com>
2021-04-21 16:41:48 +0300

    dropdown_list_page.py created should_be_elements 1/2

--- pages/input_forms/dropdown_list_page.py
+++ pages/input_forms/dropdown_list_page.py
@@ -4,6 +4,23 @@ from selenium.webdriver.support.select import Select
 
 
 class DropDownListPage(BasePage):
+    def should_be_elements(self):
+        self.should_be_select_day()
+        self.should_be_select_day_text()
+        self.should_be_undisplayed_select_day_text()
+
+    def should_be_select_day(self):
+        assert self.is_element_present(*DropDownListLocators.SELECT_DAY), \
+            "Select day is not present, but should"
+
+    def should_be_select_day_text(self):
+        assert self.is_element_present(*DropDownListLocators.DAY_SELECTED_TEXT), \
+            "Select day text is not present, but should"
+
+    def should_be_undisplayed_select_day_text(self):
+        assert not self.is_element_displayed(*DropDownListLocators.DAY_SELECTED_TEXT), \
+            "Select day text is  displayed, but should not"
+
     def quest_one(self):
         select_day = Select(self.browser.find_element(*DropDownListLocators.SELECT_DAY))
         for day in ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]:
--- pages_tests/test_input_dropdown_list.py
+++ pages_tests/test_input_dropdown_list.py
@@ -4,6 +4,12 @@ from pages.input_forms.dropdown_list_page import DropDownListPage
 link = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html"
 
 
+def test_should_be_elements(browser):
+    page = DropDownListPage(browser, link)
+    page.open()
+    page.should_be_elements()
+
+
 def test_quest_one(browser):
     page = DropDownListPage(browser, link)
     page.open()
